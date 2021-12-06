2d7e726f89803d6bb7abd6049705f01032eaa95c
wellingtonwebber <wellingtonwebber22@gmail.com>
2021-12-05 21:24:54 -0300

    Aula12

---
+++ src/Aula12/Aula12.java
@@ -0,0 +1,30 @@
+package Aula12;
+
+public class Aula12 {
+    public static void main(String[] args) {
+
+/*        ListaLigada lista = new ListaLigada();
+        lista.adicionar("AC");
+        lista.adicionar("BA");
+        lista.adicionar("CE");
+        lista.adicionar("DF");
+        System.out.println("Tamanho: " + lista.getTamanho());
+        System.out.println("Primeiro: " + lista.getPrimeiro().getValor());
+        System.out.println("Último: " + lista.getUltimo().getValor());
+
+        for(int i=0; i < lista.getTamanho(); i++){
+            System.out.println(lista.get(i).getValor());
+        }
+
+        lista.remover("DF");
+        System.out.println("Removeu estado DF");
+        lista.adicionar("SP");
+        System.out.println("Adicionou estado SP");
+
+        System.out.println("Tamanho: " + lista.getTamanho());
+        for(int i=0; i < lista.getTamanho(); i++){
+            System.out.println(lista.get(i).getValor());
+        }
+*/
+    }
+}
---
+++ src/Aula12/Pilha.java
@@ -0,0 +1,18 @@
+package Aula12;
+
+public class Pilha {
+    private ListaLigada lista;
+
+    public Pilha(){
+        this.lista = new ListaLigada();
+    }
+    public void adicionar(String novoValor){
+        this.lista.adicionarComeco(novoValor);
+    }
+    public void remover(){
+        this.lista.remover(this.get());
+    }
+    public String get(){
+        return this.lista.getPrimeiro().getValor();
+    }
+}
